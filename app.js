4aa132f706f7cd8c5f45102baf1d4db7c2e9e77e
Gabe Solomon <Gabe@Gabes-MacBook-Pro.local>
2017-09-20 20:23:12 -0400

    Pushing up the psuedo code from class.  I will update this more tonight so its framework is ready to add IDs, APIs, etc.

---
+++ app.js
@@ -0,0 +1,39 @@
+//*** DRINKER VIEW ***
+
+// Present option for Drinker to select a Brewer from X total Brewers
+
+// Register Drinker's selection
+
+// After selection is captured,
+//		(a) render distributionPointx locations on Google Map (with Google API)
+//		(b) render Google Reviews of each distributionPointx location
+
+// After rendering, provide option for Drinker to:
+//		(a) reset view, or
+//		(b) select a new brewer
+
+
+
+
+//*** MALTR ADMIN VIEW ***
+
+// **The 1.0 version will have an ADMIN view to enter data for the brewers.  Consider future versions to have Brewer logins so they can update this themselves.
+
+// Present maltr Admin with a form so they can add data of a single distribution Point where the brewer's beers can be found.  Consider future versions to allow matlr Admin the ability to edit an existing distribution Point.
+
+//		(a) Brewer Name
+//		(b) distributionPointx Name (name where Drinker can buy the beer)
+//		(c) distributionPointx Address 1
+//		(d) distributionPointx Address 2
+//		(e) distributionPointx City
+//		(f) distributionPointx State
+//		(g) distributionPointx Zip
+//		(h) distributionPointx Country
+
+// Provide maltr Admin a "Submit" button
+// Provide maltry Admin a feedback loop (visual) to display back what they entered
+
+
+
+
+
