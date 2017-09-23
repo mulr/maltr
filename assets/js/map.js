373f6d52e272e4018c56e12cbd21827c0d1797ac
Daniel Muller <DMuller@DM.local>
2017-09-23 12:51:57 -0400

    Updated map, integrate Firebase, loop

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,42 +1,140 @@
+//FIREBASE - from Gabe:
+  var config = {
+      apiKey: "AIzaSyD9zk9bTMjEZ9sqWATtCz0QI2TQ48InrVE",
+      authDomain: "maltr-2096b.firebaseapp.com",
+      databaseURL: "https://maltr-2096b.firebaseio.com",
+      projectId: "maltr-2096b",
+      storageBucket: "maltr-2096b.appspot.com",
+      messagingSenderId: "12160681601"
+  	};
+
+  firebase.initializeApp(config);
+
+  var db = firebase.database();
+
+  db.ref().on("value", function(snapshot) {
+
+    // Log everything that's coming out of snapshot
+    console.log(snapshot.val());
+    console.log(snapshot.val().brewer);
+
+  });
+//Firebase end, begin other code:
+//---------------------------------------
 
 
+// function initMap() {
+//
+//   var map = new google.maps.Map(document.getElementById("map_canvas"), {
+//     center: {lat: 28.5444896, lng: -81.3810539},
+//     zoom: 10
+//   });
+//
+//   var infowindow = new google.maps.InfoWindow();
+//   var service = new google.maps.places.PlacesService(map_canvas);
+//
+//   service.getDetails({
+//     placeId: 'ChIJWx4Z85R654gRnDxKft1rD3Q'
+//   }, function(place, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//       var marker = new google.maps.Marker({
+//         map: map,
+//         position: place.geometry.location
+//       });
+//       google.maps.event.addListener(marker, 'click', function() {
+//         infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+//           'Place ID: ' + place.place_id + '<br>' +
+//           place.formatted_address + '</div>');
+//         infowindow.open(map, this);
+//       });
+//     }
+//   });
+// }
 
-      var map;
-      var marker;
-      var myLatLng = {lat: -25.363, lng: 131.044};
+  function initMap() {
+    //Need reference firebase =
 
-      function initMap() {
-        var orlando = new google.maps.LatLng(28.5444896, -81.3810539);
-        var orlandoBrewing = new google.maps.LatLng(28.5247212, -81.3909102);
+    var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
 
+    var orlando = new google.maps.LatLng(28.5444896, -81.3810539);
+    var orlandoBrewing = new google.maps.LatLng(28.5247212, -81.3909102);
 
-        map = new google.maps.Map(document.getElementById('map_canvas'), {
-          // center: {lat: 28.5444896, lng: -81.3810539},
-          center: orlando,
-          zoom: 10
-        });
+    map = new google.maps.Map(document.getElementById('map_canvas'), {
+      // center: {lat: 28.5444896, lng: -81.3810539},
+      center: orlando,
+      zoom: 10
+    });
 
-        //What's in the text box:
-        var contentString = "<p>Four loko williamsburg art party readymade meggings, kombucha selvage sriracha glossier mlkshk humblebrag four dollar toast ethical. Messenger bag health goth forage la croix. Affogato truffaut banh mi beard semiotics. Hell of listicle truffaut man braid, aesthetic cloud bread iceland flannel yuccie activated charcoal ethical crucifix williamsburg. Narwhal tumblr fixie echo park. Glossier venmo normcore, 8-bit tote bag gluten-free before they sold out butcher knausgaard banjo chia synth. Pabst single-origin coffee whatever four loko. IPhone post-ironic cornhole 3 wolf moon wayfarers. Fingerstache PBR&B copper mug keffiyeh pop-up church-key neutra ramps distillery pickled umami bicycle rights cronut. Authentic PBR&B brunch tilde venmo, pok pok plaid. Meditation street art gastropub franzen edison bulb, messenger bag hell of health goth stumptown deep v.</p>";
+    var infowindow = new google.maps.InfoWindow();
 
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
+    var marker, i;
 
-        //Puts a marker on map:
-        marker = new google.maps.Marker({
-          position: orlandoBrewing,
-          map: map,
-          title: 'Hello World!'
-        });
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
 
-        //Opens the text box when we click:
-        marker.addListener('click', function() {
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
           infowindow.open(map, marker);
-        });
+        }
+      })(marker, i));
+    }
+  }
 
-      }
 
+
+
+
+
+
+//map1
+//----------------------------------------
+      // var map;
+      // var marker;
+      // var myLatLng = {lat: -25.363, lng: 131.044};
+      //
+      // function initMap() {
+      //   var orlando = new google.maps.LatLng(28.5444896, -81.3810539);
+      //   var orlandoBrewing = new google.maps.LatLng(28.5247212, -81.3909102);
+      //
+      //
+      //   map = new google.maps.Map(document.getElementById('map_canvas'), {
+      //     // center: {lat: 28.5444896, lng: -81.3810539},
+      //     center: orlando,
+      //     zoom: 10
+      //   });
+      //
+      //   //What's in the text box:
+      //   var contentString = "<p>Four loko williamsburg art party readymade meggings, kombucha selvage sriracha glossier mlkshk humblebrag four dollar toast ethical. Messenger bag health goth forage la croix. Affogato truffaut banh mi beard semiotics. Hell of listicle truffaut man braid, aesthetic cloud bread iceland flannel yuccie activated charcoal ethical crucifix williamsburg. Narwhal tumblr fixie echo park. Glossier venmo normcore, 8-bit tote bag gluten-free before they sold out butcher knausgaard banjo chia synth. Pabst single-origin coffee whatever four loko. IPhone post-ironic cornhole 3 wolf moon wayfarers. Fingerstache PBR&B copper mug keffiyeh pop-up church-key neutra ramps distillery pickled umami bicycle rights cronut. Authentic PBR&B brunch tilde venmo, pok pok plaid. Meditation street art gastropub franzen edison bulb, messenger bag hell of health goth stumptown deep v.</p>";
+      //
+      //   var infowindow = new google.maps.InfoWindow({
+      //     content: contentString
+      //   });
+      //
+      //   //Puts a marker on map:
+      //   marker = new google.maps.Marker({
+      //     position: orlandoBrewing,
+      //     map: map,
+      //     title: 'Hello World!'
+      //   });
+      //
+      //   //Opens the text box when we click:
+      //   marker.addListener('click', function() {
+      //     infowindow.open(map, marker);
+      //   });
+      //
+      // }
+//---------------------------------------------------
 
 
       // $("#map_canvas").hide();
