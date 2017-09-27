ef8c8255b8ffa23119b91a9ecf59a27908abbb85
Eric Matson <matson@Erics-MacBook-Pro.local>
2017-09-27 00:52:51 -0400

    changes madd to map.js integrating firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,6 +62,9 @@ var database = firebase.database();
 
 // PROCESSES
 
+https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+
+
 $(".dropdown-item").on('click', function() {
     // event.preventDefault();
 
